6aa1ef48c0145d3733ea071206363981f95b4226
SarielGil <45231074+SarielGil@users.noreply.github.com>
2020-02-16 00:48:35 +0200

    DF styling has been implemented on the html side

--- flask_display_ner.py
+++ flask_display_ner.py
@@ -1,45 +1,17 @@
 
 
-from flask import Flask, render_template, url_for, request, jsonify, Markup
+from flask import Flask, render_template, request, Markup
 from spacy import displacy
-import re
 import pandas as pd
 import spacy
-import json
-from spacy import displacy
-from flask_cors import CORS, cross_origin
-from flaskext.markdown import Markdown
+from flask_cors import CORS
 
 
 nlp = spacy.load('en_core_web_sm')
 
 app = Flask(__name__)
 cors = CORS(app)
-# Markdown(app)
 
-
-
-def style_df(df):
-    # Set CSS properties for th elements in dataframe
-    th_props = [
-        ('font-size', '11px'),
-        ('text-align', 'center'),
-        ('font-weight', 'bold'),
-        ('color', '#6d6d6d'),
-        ('background-color', '#f7f7f9')
-    ]
-    # Set CSS properties for td elements in dataframe
-    td_props = [
-        ('font-size', '11px')
-    ]
-    # Set table styles
-    styles = [
-        dict(selector="th", props=th_props),
-        dict(selector="td", props=td_props)
-    ]
-    (df.style
-     .set_table_styles(styles))
-    return df
 
 app = Flask(__name__)
 
@@ -63,15 +35,12 @@ def process():
             d.append((ent.label_, ent.text))
             df = pd.DataFrame(d, columns=('named entity', 'output'))
         df1 = df.pivot(columns='named entity', values='output').fillna('')
-        df1 = style_df(df1)
-        # print(df1)
-        # print(UI_results)
         return render_template("new_htm.html", UI_results=Markup(UI_results), table_result=Markup(df1.to_html(classes='data'
                                                                                                        ,header="true")))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
 
 
 # Result:
